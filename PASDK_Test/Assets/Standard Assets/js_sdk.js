960683d1968e3453e2285ac1dd812c001cefa27f
Neel Heo <neel.heo@mbizglobal.kr>
2018-04-26 11:14:51 +0900

    update

--- PASDK_Test/Assets/Standard Assets/js_sdk.js
+++
@@ -1,9 +0,0 @@
-﻿public var testmessage : String = "This is JavaScript Message!!";
-public function TestFunc1(){
-    return "TestFunc!!";
-}
-
-public function TestFunc2(a:String)
-{
-    Debug.Log(a);
-}
