b36bd4e2b4e19289643e24370a4c935f49b628ab
emojigames <38741669+emojigames@users.noreply.github.com>
2018-04-26 10:45:58 +0900

    Add files via upload

---
+++ pasdk/unity/PASDK_Test/Assets/Standard Assets/js_sdk.js
@@ -0,0 +1,9 @@
+﻿public var testmessage : String = "This is JavaScript Message!!";
+public function TestFunc1(){
+    return "TestFunc!!";
+}
+
+public function TestFunc2(a:String)
+{
+    Debug.Log(a);
+}
---
+++ pasdk/unity/PASdkLib/PASdkLib/src/Cmn_Var.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using LitJson;
+
+namespace PASdkLib.src
+{
+    class Cmn_Var
+    {
+        public static bool isInit = false;
+        public static String gameID = "default";
+        public static String KEY_USER_ID = "user_id";
+        public static String KEY_USER_TOKEN = "userToken";
+        public static long playerPocBalance = 0;
+        public static String playLogoNo = "";
+        public static String url = "https://pa.emojigames.io";
+        public static String gameApiUrl = "https://pa.emojigames.io/ssapi/";
+        public static String gameApiExt = ".php";
+        public static String apimember = "/member/";
+        public static String apipoc = "/poc/";
+        public static JsonData game = null;
+        public static JsonData playerInfo = null;
+        public static JsonData user = null;
+        public static JsonData localKeys = null;
+        public static JsonData initmsg = new JsonData();
+
+    }
+}
